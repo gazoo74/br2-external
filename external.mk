964514f890e67f2180e3a2fdd51b8ecc611ab47f
Gaël PORTAY <gael.portay@savoirfairelinux.com>
2018-02-11 12:36:29 -0500

    Add hook to copy local wpa_supplicant conffiles

--- external.mk
+++ external.mk
@@ -1,1 +1,10 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_GPORTAY_PATH)/package/*/*.mk))
+
+update-wpa-supplicant:
+	if [ -d /etc/wpa_supplicant/ ]; then \
+		$(call MESSAGE,"Update WPA Supplicant configuration hook"); \
+		rsync -av /etc/wpa_supplicant/*.conf \
+		          $(BR2_EXTERNAL_GPORTAY_PATH)/board/gportay/overlay/etc/wpa_supplicant/; \
+	fi
+
+target-finalize: update-wpa-supplicant
