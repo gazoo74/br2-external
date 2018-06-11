5fb14ececbccda091c528e3f31aeb8cb233f8340
Gaël PORTAY <gael.portay@savoirfairelinux.com>
2018-06-11 14:15:33 -0400

    Fix update-wpa-supplicant when directory is empty

--- external.mk
+++ external.mk
@@ -4,7 +4,7 @@ include $(sort $(wildcard $(BR2_EXTERNAL_GPORTAY_PATH)/package/*/*.mk))
 update-wpa-supplicant:
 	if [ -d /etc/wpa_supplicant/ ]; then \
 		$(call MESSAGE,"Update WPA Supplicant configuration hook"); \
-		rsync -av /etc/wpa_supplicant/*.conf \
+		rsync -av /etc/wpa_supplicant/. \
 		          $(BR2_EXTERNAL_GPORTAY_PATH)/board/gportay/overlay/etc/wpa_supplicant/; \
 	fi
 
