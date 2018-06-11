c332671752d40582e93cef9393a2eb6c93cd576a
Gaël PORTAY <gael.portay@savoirfairelinux.com>
2018-06-11 14:14:24 -0400

    Make update-wpa-supplicant a .PHONY target

--- external.mk
+++ external.mk
@@ -1,5 +1,6 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_GPORTAY_PATH)/package/*/*.mk))
 
+.PHONY: update-wpa-supplicant
 update-wpa-supplicant:
 	if [ -d /etc/wpa_supplicant/ ]; then \
 		$(call MESSAGE,"Update WPA Supplicant configuration hook"); \
